4daf0e685f4409a23b20374f28a3e884a8530e64
Noah <yasskin@icloud.com>
2016-06-12 19:55:45 -0700

    set up initial files html css javascript and readme

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Business (or back-end) logic:
+
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+
+    event.preventDefault();
+});
+
+
+var testGreeting = newString;
+var testGreeting2 = new String("Hello!");
+
+//constructor function
+function Dog(name, colors, age) {
+  this.name = name;
+  this.colors = colors;
+  this.age = age;
+}
+
+// my puppy is an 'instance' of the 'Dog' type
+var myPuppy - new Dog("Ernie", ["brown", "black"], 3);
+myPuppy.name;
+myPuppy.colors;
+
+//prototypes
+String.prototype.addExcitement = function() {return this + "!!!!"};
+Dog.prototype.speak = function() {
+  console.log("Woof!");
+}
