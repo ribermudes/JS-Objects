7914f2ca9565b3be4c13b56cbe32c56d65d8f909
Noah Yasskin and Rachael Bermudes <rachael@>
2016-06-14 14:57:34 -0700

    create movie ticket prices

--- js/tickets.js
+++ js/tickets.js
@@ -6,15 +6,28 @@ function Ticket(feature, time, age) {
   this.age = age;
 }
 
+function ticketCost () {
+  return ticketPrice = feature.value + time.value + age.value;
+}
+
+
 // user interface
 
 $(document).ready(function() {
   $("form#tickets").submit(function(event) {
     event.preventDefault();
 
-    var features = $("input[name=features]:checked").val();
-    var time = $("input[name=time]:checked").val();
-    var age = $("input[name=age]:checked").val();
+    var inputtedFeatures = parseInt($("#film-choices").val());
+    var inputtedTimes = parseInt($("#time-choices").val());
+    var inputtedAges = parseInt($("#age-choices").val());
 
+    var ticketPrice = new Ticket(inputtedFeatures, inputtedTimes, inputtedAges);
+
+    var displayPrice = ticketPrice.feature + ticketPrice.time + ticketPrice.age;
+    console.log(displayPrice);
+    // var newTicket = new Ticket(features, time, age);
+    // console.log(newTicket);
+    $("#tickets-output").show();
+    $("#tickets-output").text(displayPrice);
   });
 });
